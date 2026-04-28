c7f58337fa5b356f22602372347eae077bd8e9e8
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-04-28 23:24:12 +0900

    chore: 좋아요 취소 중복 에러 코드 메시지 완성

--- src/main/java/org/sopt/post/code/PostErrorCode.java
+++ src/main/java/org/sopt/post/code/PostErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum PostErrorCode implements ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
     POST_ALREADY_LIKED(HttpStatus.CONFLICT, "이미 좋아요를 누른 게시물입니다."),
-    POST_ALREADY_UNLIKED(HttpStatus.CONFLICT, "좋아요를 누르지 않은 게시물은 ");
+    POST_ALREADY_UNLIKED(HttpStatus.CONFLICT, "좋아요를 누르지 않은 게시물은 취소할 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
